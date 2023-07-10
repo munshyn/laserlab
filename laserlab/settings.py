379be0daa987dc40dacc320b8b4d1e14889ba614
munshyn <husny.sk@gmail.com>
2023-07-10 08:55:49 +0800

    reorganize hierarchy of files

--- laserlab/settings.py
+++ laserlab/settings.py
@@ -153,7 +153,7 @@ AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
-AWS_S3_CUSTOM_DOMAIN = os.getenv('AWS_S3_CUSTOM_DOMAIN')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 # Configure static files storage if needed
 STATICFILES_STORAGE = 'laserlab.storage_backends.S3StaticStorage'
